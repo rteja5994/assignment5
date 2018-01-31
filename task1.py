2ca98a814ca3d64bb0d05258091299696748d35c
rteja5994 <raviteja.chunduri@dal.ca>
2018-01-31 09:35:20 -0400

    Initialised class and program to check the ip address

--- task1.py
+++ task1.py
@@ -1,3 +1,16 @@
+import math
+
 class Network_Subnetting:
 
-	def __init__(self):
+	def __init__(self,ip,prefix):
+		self.net_address = []
+		self.prefix = []
+		ip = ip.split(".")
+		ad = "1"*prefix + "0"*[32-prefix]
+		octet = (ad[0:8],ad[8:16],ad[16:24],ad[24:32])
+
+	
+	for p in range(len(ip)):
+                a = int(ip[p])
+                b = int(octet[p], 2)
+                self.net_addr.append(a&b)	
