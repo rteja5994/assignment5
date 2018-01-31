b872453b2f47fd7bd79c728765dabbb4e8508002
rteja5994 <raviteja.chunduri@dal.ca>
2018-01-31 09:46:05 -0400

    Initilasing the subnet function and splitting the host network to subnet

--- task1.py
+++ task1.py
@@ -13,4 +13,13 @@ class Network_Subnetting:
 	for p in range(len(ip)):
                 a = int(ip[p])
                 b = int(octet[p], 2)
-                self.net_addr.append(a&b)	
+                self.net_address.append(a&b)	
+
+	print ("Network Address:", self.net_addr)
+
+	def subnet(self):
+		c = int(math.log(count,2))
+		tar = self.prefix + c
+		ad = "1"*tar + "0"*[32-tar]
+		octet = (ad[0:8],ad[8:16],ad[16:24],ad[24:32])
+		print (octet)
